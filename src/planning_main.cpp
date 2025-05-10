740cbd176f458ee17cc0ec4e6e42aa6f48b7e797
junhua-yao <13727289085@163.com>
2025-05-10 22:22:08 +0800

    add third_party

--- src/planning_main.cpp
+++ src/planning_main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include"process.h"
+#include"show_result.h"
 using std::cout,std::endl;
 int main()
 {
@@ -7,5 +8,9 @@ int main()
     Process pro;
     pro.plan_process();
     cout <<"planning end"<<endl;
+
+    cout<<"show result: "<<endl;
+    ShowResult show;
+    show.drawResult();
     return 0;
 }
